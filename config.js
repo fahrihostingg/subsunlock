5e18ce05e46450cfbd4c8173573f702b2c239a73
fahrihostingg <150719442+fahrihostingg@users.noreply.github.com>
2024-09-23 09:28:23 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const config = {
+    mainTitle: "Buka Kunci Url",
+    mainText: "Klik butang di bawah untuk menyukai video dan melanggan saluran.",
+    likeUrl: "https://youtu.be/@rulshz",
+    subscribeUrl: "https://youtu.be/@rulshz",
+    button1Text: "Sc Campuran V9",
+    button1Url: "https://www.subs4unlock.id/9671fX",
+    button2Text: "Hw V20",
+    button2Url: "https://www.subs4unlock.id/IUoCYz",
+    button3Text: "Sc Silvia MD",
+    button3Url: "https://www.subs4unlock.id/qbobeG",
+    slideshowImages: [
+        "https://telegra.ph/file/b3472eb5c741baa6a9e56.jpg",
+        "https://telegra.ph/file/54e758199b3faef966e24.jpg"
+    ]
+};
